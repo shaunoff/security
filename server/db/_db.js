43510164193d0b4c051d282fb4abf52cf320efbb
Shaun Hutch <shaunoff@Shauns-MBP.home>
2018-01-01 19:20:16 -0500

    database url

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const databaseURI = 'postgres://localhost:5432/auth';
-
+//const databaseURI = 'postgres://localhost:5432/auth';
+const databaseURI = DATABASE_URL
 const db = new Sequelize(databaseURI, {
   define: {
     underscored: true
